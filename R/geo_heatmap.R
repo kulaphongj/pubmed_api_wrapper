5ce62d416ba12a2d02957a73aa451906d23fb7d7
kulaphongj <kulaphong.j@gmail.com>
2024-01-28 20:49:55 -0800

    rearrange folder structure for packaging and testing

--- R/geo_heatmap.R
+++ R/geo_heatmap.R
@@ -4,13 +4,13 @@ library(dplyr)
 library(tools)
 library(plotly)
 
-# Define the main domain of the url
-domain <- "https://api.yelp.com/v3"
-
-# Define the Authorization token
-# token <- ""
-
-# Create a dictionary to hold the parameters for the API request
+# # Define the main domain of the url
+# domain <- "https://api.yelp.com/v3"
+# 
+# # Define the Authorization token
+# # token <- ""
+# 
+# # Create a dictionary to hold the parameters for the API request
 params <- list(
   location = 'Vancouver',
   categories = 'restaurant',
@@ -18,57 +18,57 @@ params <- list(
   sort_by = 'best_match',
   limit = 50
 )
-
-# Define the URL for businesses search endpoint
-url_businesses <- paste0(domain, "/businesses/search?")
-
-# Request the data
-response <- GET(url_businesses, add_headers(Authorization = token), query = params)
-
-# Check if the request was successful
-if (status_code(response) != 200) {
-  stop("Failed to retrieve data.")
-}
-
-# Extract the requested data
-raw_data <- content(response, "text") %>%
-  fromJSON(simplifyVector = TRUE) %>%
-  `[[`("businesses")
-
-# Create a DataFrame
-df_businesses <- as.data.frame(raw_data)
-
-# Select the columns of interest
-df_ratings <- df_businesses %>%
-  dplyr::select(name, review_count, rating, distance, price, coordinates)
-
-df_ratings$latitude <- df_ratings$coordinates$latitude
-df_ratings$longitude <- df_ratings$coordinates$longitude
-df_ratings = select(df_ratings, -c(coordinates))
-
-# Convert numerical columns to numeric format
-numeric_columns <- c('review_count', 'rating', 'distance', 'latitude', 'longitude')
-df_ratings[numeric_columns] <- lapply(df_ratings[numeric_columns], as.numeric)
-
-
-df_ratings <- df_ratings %>%
-  arrange(name) %>%
-  group_by(name) %>%
-  mutate(RunningNumber = row_number())
-
-# Add a number to a string value when Value > 1
-df_ratings <- df_ratings %>%
-  mutate(name = ifelse(RunningNumber > 1, paste(name, " ", RunningNumber, sep = ""), name))
-
-
-
-# Map price levels to factors
-price_factors <- c("$" = 1, "$$" = 2, "$$$" = 3, "$$$$" = 4)
-price_factors[is.na(price_factors)] <- NULL
-
-# Add price factor column
-df_ratings$price_factor <- price_factors[df_ratings$price]
-df_ratings$price_factor[is.na(df_ratings$price_factor)] <- 0.1
+# 
+# # Define the URL for businesses search endpoint
+# url_businesses <- paste0(domain, "/businesses/search?")
+# 
+# # Request the data
+# response <- GET(url_businesses, add_headers(Authorization = token), query = params)
+# 
+# # Check if the request was successful
+# if (status_code(response) != 200) {
+#   stop("Failed to retrieve data.")
+# }
+# 
+# # Extract the requested data
+# raw_data <- content(response, "text") %>%
+#   fromJSON(simplifyVector = TRUE) %>%
+#   `[[`("businesses")
+# 
+# # Create a DataFrame
+# df_businesses <- as.data.frame(raw_data)
+# 
+# # Select the columns of interest
+# df_ratings <- df_businesses %>%
+#   dplyr::select(name, review_count, rating, distance, price, coordinates)
+# 
+# df_ratings$latitude <- df_ratings$coordinates$latitude
+# df_ratings$longitude <- df_ratings$coordinates$longitude
+# df_ratings = select(df_ratings, -c(coordinates))
+# 
+# # Convert numerical columns to numeric format
+# numeric_columns <- c('review_count', 'rating', 'distance', 'latitude', 'longitude')
+# df_ratings[numeric_columns] <- lapply(df_ratings[numeric_columns], as.numeric)
+# 
+# 
+# df_ratings <- df_ratings %>%
+#   arrange(name) %>%
+#   group_by(name) %>%
+#   mutate(RunningNumber = row_number())
+# 
+# # Add a number to a string value when Value > 1
+# df_ratings <- df_ratings %>%
+#   mutate(name = ifelse(RunningNumber > 1, paste(name, " ", RunningNumber, sep = ""), name))
+# 
+# 
+# 
+# # Map price levels to factors
+# price_factors <- c("$" = 1, "$$" = 2, "$$$" = 3, "$$$$" = 4)
+# price_factors[is.na(price_factors)] <- NULL
+# 
+# # Add price factor column
+# df_ratings$price_factor <- price_factors[df_ratings$price]
+# df_ratings$price_factor[is.na(df_ratings$price_factor)] <- 0.1
 
 
 # GeoHeatmap ============================================================================================================
@@ -145,18 +145,18 @@ geo_heatmap <- function(df_loc, factor_plot) {
   return(fig) 
 }
 
-# call function
-test <-  df_ratings[df_ratings$review_count<500, ]
-test <- df_ratings
-
-fig_geo_heat <- geo_heatmap(test, factor_plot='price_factor')
-fig_geo_heat
-
-
-fig_geo_heat <- geo_heatmap(test, factor_plot='rating')
-fig_geo_heat
-
-
-fig_geo_heat <- geo_heatmap(test, factor_plot='review_count')
-fig_geo_heat
-
+# # call function
+# test <-  df_ratings[df_ratings$review_count<500, ]
+# test <- df_ratings
+# 
+# fig_geo_heat <- geo_heatmap(test, factor_plot='price_factor')
+# fig_geo_heat
+# 
+# 
+# fig_geo_heat <- geo_heatmap(test, factor_plot='rating')
+# fig_geo_heat
+# 
+# 
+# fig_geo_heat <- geo_heatmap(test, factor_plot='review_count')
+# fig_geo_heat
+# 
