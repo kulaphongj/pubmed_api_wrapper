52ee251fb55b15b0633b8a464261cde65eb5682f
Nijat27 <nabliz@student.ubc.ca>
2024-01-29 23:33:15 -0800

    updated log

--- R/SearchBusinesses.R
+++ R/SearchBusinesses.R
@@ -4,7 +4,27 @@ library(jsonlite)
 
 api_key = '8Jo6kJG76tJHDooxSbrcpFOm1dW2DTKJBQ_sNZuqHDn5FMmNTHe4xTuJXSblClvr5o6mKo4x2YzHsOonykbo7ajvVvWzGmjK0SpVxfGXmXqDWPu9Qivdyx-OjyewZXYx'
 
-# Function to search for businesses on Yelp using the Yelp Fusion API
+
+#' Search for businesses on Yelp using the Yelp Fusion API
+#'
+#' This function allows you to search for businesses on Yelp using the Yelp Fusion API.
+#'
+#' @param api_key Your Yelp Fusion API key.
+#' @param location The location where you want to search for businesses (e.g., 'Kelowna').
+#' @param business_type The type of business you are looking for (e.g., 'restaurants').
+#' @param keyword A keyword to narrow down the search (e.g., 'sushi').
+#' @param offset The offset for paginating through results (default is 0).
+#' @param limit The maximum number of results to return (default is 50).
+#'
+#' @return A list of businesses matching the search criteria.
+#'
+#' @examples
+#' api_key <- 'your_api_key'  # Replace with your actual API key
+#' results <- SearchBusinesses(api_key, 'Kelowna', 'restaurants', 'sushi', offset = 0, limit = 50)
+#'
+#' @seealso \code{\link{httr::GET}}, \code{\link{httr::add_headers}}, \code{\link{jsonlite::fromJSON}}
+#'
+#' @export
 SearchBusinesses <- function(api_key, location, business_type, keyword, offset = 0, limit = 50) {
   # Define the API endpoint URL
   url <- 'https://api.yelp.com/v3/businesses/search'
